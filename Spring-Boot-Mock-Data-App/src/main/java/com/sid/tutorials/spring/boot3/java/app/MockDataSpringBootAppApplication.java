cf6b4daa17f26a5472614dcc3e47265875a12b85
javawithinfinitepossibilities@gmail.com <javawithinfinitepossibilities@gmail.com>
2024-08-05 17:18:47 +0530

    Initial commit for Java Stream API

---
+++ Spring-Boot-Mock-Data-App/src/main/java/com/sid/tutorials/spring/boot3/java/app/MockDataSpringBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.sid.tutorials.spring.boot3.java.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kunmu On 05-08-2024
+ */
+@SpringBootApplication
+public class MockDataSpringBootAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MockDataSpringBootAppApplication.class, args);
+    }
+}
